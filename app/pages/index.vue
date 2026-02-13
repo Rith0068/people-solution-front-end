65902332769db981cc78f5e6ecae54242159e9e5
Your Name <your_email@example.com>
2026-02-13 14:52:02 +0700

    update about

---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <link rel="stylesheet" href="/style.css">
+  <div class="p-10 text-center">
+    <h1 class="text-4xl font-bold text-pink-500">
+      Tailwind is working 🎉
+    </h1>
+    <p class="mt-4 text-gray-500">
+      If this is pink and big, you’re good.
+    </p>
+  </div>
+</template>
