264b9da674d351372af67b77da49dc481ebc34a7
sokcheadalinsorn <sokcheadalinsorn@gmail.com>
2026-02-16 15:11:47 +0700

    publish branch

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <RouterView />
+    
+
   </div>
 </template>
---
+++ app/pages/service.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="bg-gray-100 ">
+    <!-- hero section service -->
+    <div class="">
+      <!-- Image -->
+      <img src="../assets/image/bg-service.jpg" alt="Sample Image" class="w-full h-[300px] object-cover" />
+
+      <!-- Text Overlay -->
+      <div class="absolute inset-0 flex mb-[700px] items-center justify-center">
+        <h1 class="text-white font-bold text-6xl font-bold p-4  bg-opacity-50">
+          OUR SERVICE
+        </h1>
+      </div>
+    </div>
+
+    <!-- service intoduce section -->
+    <div class="   w-4/5 m-auto mt-[90px] flex justify-between gap-[30px]">
+      <div class="w-4/5 m-auto">
+        <button
+          class="text-sm font-semibold inline-block uppercase tracking-wind px-3 py-1 bg-[#E9C769] rounded-full">Our
+          Service</button>
+        <h2 class="te mt-[30px] text-4xl lg:text-5xl fond-bold leading-light ">
+          Premium HR Services Tailored for Cambodia.
+        </h2>
+      </div>
+
+      <p class="text-md mt-[60px] text-gray-600 leading-relaxed ">
+        We deliver expert recruitment, payroll, and HR solutions—helping your business grow with trusted support and
+        local
+        expertise. Our comprehensive approach ensures seamless integration of HR best practices tailored specifically
+        for
+        the Cambodian market.
+      </p>
+    </div>
+
+    <!-- service card section -->
+    <section class="py-30 slide-up-animation animate-in data-scroll-section w-full">
+      <div class="max-w-7xl m-auto px-4 sm:px-6 lg:px-8">
+        <!-- each card -->
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div
+            class="bg-white-500 rounded-2xl p-8 shadow-lg hover:shadow-lg transform transition hover:-translate-y-4 hover:scale-110duration-300 ease-out ">
+            <div class="text-left space-y-4 fles-grow">
+              <div class="w-14 h-14 flex items-center justify-center">
+                <img class="-10 h-10 fill-none strock-currentColor" src="../assets/image/growth.png" alt="investment">
+              </div>
+              <h3 class="text-lg font-semibold text-black">PEO / EOR Partnership Services</h3>
+              <p class="text-gray-600 text-sm leading-relaxed">As your local PEO provider, we provide seamless expertise
+                for
+                global PEOs and EORs in Cambodia, ensuring your local operations are fully compliant and expertly
+                managed.
+              </p>
+            </div>
+            <div class="mt-auto pt-6 ">
+              <a class="bg-[rgb(233,219,105)] inline-block px-6 py-2 rounded-full font-md transition-duration-300 ease-out hover-scale-105 hover:shadow-lg text-sm"
+                href="">
+                Read More
+              </a>
+            </div>
+          </div>
+
+          <div
+            class="bg-white-500 rounded-2xl p-8 shadow-lg hover:shadow-lg transform transition hover:-translate-y-4 hover:scale-110duration-300 ease-out ">
+            <div class="text-left space-y-4 fles-grow">
+              <div class="w-14 h-14 flex items-center justify-center">
+                <img class="-10 h-10 fill-none strock-currentColor" src="../assets/image/coin.png" alt="money">
+              </div>
+              <h3 class="text-lg font-semibold text-black">Payroll & National Social Security Fund (NSSF)</h3>
+              <p class="text-gray-600 text-sm leading-relaxed">AEliminate expensive mistakes. Our payroll & NSSF
+                calculation services provide accuracy, timely disbursements, and full compliance to all local tax and
+                NSSF requirements every payroll cycle.
+              </p>
+            </div>
+            <div class="mt-auto pt-6 ">
+              <a class="bg-[rgb(233,219,105)] inline-block px-6 py-2 rounded-full font-md transition-duration-300 ease-out hover-scale-105 hover:shadow-lg text-sm"
+                href="">
+                Read More
+              </a>
+            </div>
+          </div>
+
+          <div
+            class="bg-white-500 rounded-2xl p-8 shadow-lg hover:shadow-lg transform transition hover:-translate-y-4 hover:scale-110duration-300 ease-out  ">
+            <div class="text-left space-y-4 fles-grow">
+              <div class="w-14 h-14 flex items-center justify-center">
+                <img class="-10 h-10 fill-none strock-currentColor" src="../assets/image/check-mark.png"
+                  alt="check-mark.png">
+              </div>
+              <h3 class="text-lg font-semibold text-black">Labor Law Compliance & Registration</h3>
+              <p class="text-gray-600 text-sm leading-relaxed">We keep your company compliant with Cambodian Labor Law
+                and NSSF regulations. Need to register your company? We make the registration process simple for you.
+              </p>
+            </div>
+            <div class="mt-auto pt-6 ">
+              <a class="bg-[rgb(233,219,105)] inline-block px-6 py-2 rounded-full font-md transition-duration-300 ease-out hover-scale-105 hover:shadow-lg text-sm"
+                href="">
+                Read More
+              </a>
+            </div>
+          </div>
+
+          <div
+            class="bg-white-500 rounded-2xl p-8 shadow-lg hover:shadow-lg transform transition hover:-translate-y-4 hover:scale-110duration-300 ease-out  ">
+            <div class="text-left space-y-4 fles-grow">
+              <div class="w-14 h-14 flex items-center justify-center">
+                <img class="-10 h-10 fill-none strock-currentColor" src="../assets/image/growth.png" alt="investment">
+              </div>
+              <h3 class="text-lg font-semibold text-black">Staffing and Recruitment</h3>
+              <p class="text-gray-600 text-sm leading-relaxed">We provide strategic recruitment services to bring you
+                the best local and foreign talent in the Kingdom of Cambodia.
+              </p>
+            </div>
+            <div class="mt-auto pt-6 ">
+              <a class="bg-[rgb(233,219,105)] inline-block px-6 py-2 rounded-full font-md transition-duration-300 ease-out hover-scale-105 hover:shadow-lg text-sm"
+                href="">
+                Read More
+              </a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
+  </div>
+
+</template>
